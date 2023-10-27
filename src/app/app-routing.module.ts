99f512a2186d3d7e02cac09e9429376368669152
aslexander14 <aslexander14@gmail.com>
2023-10-27 13:40:26 -0500

    Se agrega pj

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MenuComponent } from './menu/menu.component';
+import { NosotrosComponent } from './nosotros/nosotros.component';
+import { HomeComponent } from './home/home.component';
+import { PromocionesComponent } from './promociones/promociones.component';
+import { CartaComponent } from './carta/carta.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'menu', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'nosotros', component: NosotrosComponent },
+  { path: 'promociones', component: PromocionesComponent },
+  { path: 'carta', component: CartaComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './menu/menu.component';
+import { PromocionesComponent } from './promociones/promociones.component';
+import { FooterComponent } from './footer/footer.component';
+import { NosotrosComponent } from './nosotros/nosotros.component';
+import { CardComponent } from './card/card.component';
+import { BodyComponent } from './body/body.component';
+import { CartaComponent } from './carta/carta.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MenuComponent,
+    PromocionesComponent,
+    FooterComponent,
+    NosotrosComponent,
+    CardComponent,
+    BodyComponent,
+    CartaComponent
   ],
   imports: [
     BrowserModule,
